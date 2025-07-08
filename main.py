35177e9b7539e82ad2bd3c62d687d42c5ee9ea05
Ethan Numan <ehunternuman@gmail.com>
2025-07-08 13:22:00 -0400

    reconfiguration of files

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import bisect
 import heapq
 from tag import Tag
 
-CONFIG_PATH = "config.json"
+CONFIG_PATH = "./config/config.json"
 
 
-# TODO figure out how to format state machine as JSON 
+# TODO figure out how to format state machine as JSON
 def load_json(file_input):
     """Loads config file, gaining information it needs to run
 
     Returns:
-        tags,actions,default: List of information thats stored in JSON file
+        tags,events,default: List of information thats stored in JSON file
     """
 
     DEFAULT_STATS = {"exciter_power": 500.0, "gain": 0.0, "resistive_load": 50}
@@ -24,36 +24,37 @@ def load_json(file_input):
 
         if raw_data.get("Format") == "config":
             raw_objects = raw_data.get("Objects", {})
-            raw_actions = raw_data.get("Actions", [])
+            raw_events = raw_data.get("events", [])
 
             default = raw_data.get("Default")
             tags = {uid: Tag.from_dict(uid, val) for uid, val in raw_objects.items()}
-            actions = [action for action in raw_actions]
-            return None,tags, actions, default
-        elif raw_data.get("Format") == "input_machine":   
-            print("input machine loaded")  
-            return None, None, None, None                   # Placeholder 
-        elif raw_data.get("Format") == "processing_machine": 
-            print("processing machine loaded")  
-            return None, None, None, None                   # Placeholder
+            events = [event for event in raw_events]
+            return None, tags, events, default
+        elif raw_data.get("Format") == "input_machine":
+            print("input machine loaded")
+            return None, None, None, None  # Placeholder
+        elif raw_data.get("Format") == "processing_machine":
+            print("processing machine loaded")
+            return None, None, None, None  # Placeholder
         elif raw_data.get("Format") == "output_machine":
-            print("output machine loaded")  
-            return None, None, None, None                  # Placeholder
+            print("output machine loaded")
+            return None, None, None, None  # Placeholder
         else:
             print("error: invalid JSON format")
             sys.exit(1)
     elif file_input == CONFIG_PATH:
-         return None,{}, [], DEFAULT_STATS
+        return None, {}, [], DEFAULT_STATS
     else:
         print("error: file doesn't exist")
         sys.exit(1)
 
-def save_config(objects, actions, default):
+
+def save_config(objects, events, default):
     """offloads changes back to JSON file
 
     Args:
         tags (dict): Dictionary of the tags that are in the system
-        actions (list): List of actions that simulation will peform
+        events (list): List of events that simulation will peform
     """
     with open(CONFIG_PATH, "w") as f:
         json.dump(
@@ -64,10 +65,11 @@ def save_config(objects, actions, default):
                     "gain": 0,  # (dBi) Isotropic by default
                     "resistive_load": default["resistive_load"],  # (ohms)
                 },
-                "Objects": {uid: objects.to_dict() for uid, objects in objects.items()},
-                "Actions": [action for action in actions],
+                "Objects": {uid: obj.to_dict() for uid, obj in objects.items()},
+                "events": events,
             },
             f,
+            indent=4,
         )
 
 
@@ -122,13 +124,13 @@ def parse_args():
     parser.add_argument(
         "--remove", type=str, required=False, help="Remove a specific tag based on UID"
     )
-    parser.add_argument("--print", type=str, help="Arguments; actions,objects")
+    parser.add_argument("--print", type=str, help="Arguments; events,objects")
 
     parser.add_argument(
-        "--action",
+        "--event",
         nargs=4,
         metavar=("HH:MM:SS", "tx", "rx", "protocol"),
-        help="An action that will be simulated",
+        help="An event that will be simulated",
     )
     parser.add_argument(
         "--default", nargs=2, metavar=("name", "value"), help="changes a default value"
@@ -149,11 +151,11 @@ def load(filepath):
         filepath (string): text file to load in
 
     Returns:
-        objects,actions,default: information about the simulation configuration
+        objects,events,default: information about the simulation configuration
     """
 
     default = {}
-    actions = []
+    events = []
     objects = {}
     if os.path.exists(filepath):
         with open(filepath, "r") as f:
@@ -173,34 +175,38 @@ def load(filepath):
                     objects[info[1]] = tag
                 elif info[0] == "default":
                     default[info[1]] = float(info[2])
-                elif info[0] == "action":
-                    action = info[1:]
-                    times = [a[0] for a in actions]
+                elif info[0] == "event":
+                    event = info[1:]
+                    times = [e[0] for e in events]
                     position = bisect.bisect_left(times, info[1])
-                    actions.insert(position, action)
-            return objects, actions, default
+                    events.insert(position, event)
+            return objects, events, default
 
 
 def main():
 
-    machine, objects, actions, default = load_json(CONFIG_PATH)
+    machine, objects, events, default = load_json(CONFIG_PATH)
     args = parse_args()
 
     if args.load is not None:  # load in a file
-        file_type = args.load.split(".")[1]
-        if file_type=="txt":
+        file_type = args.load.split(".")[-1]
+        if file_type == "txt":
             if args.add:  # appends loaded arguments instead of overwrite
-                add_objects, add_actions, add_default = load(args.load)
+                add_objects, add_events, add_default = load(args.load)
                 objects.update(add_objects)
                 default.update(add_default)
-                actions = list(heapq.merge(actions, add_actions, key=lambda x: x[0]))
+                events = list(heapq.merge(events, add_events, key=lambda x: x[0]))
             else:  # overwrites previouse saved data
-                objects, actions, default = load(args.load)
+                objects, events, default = load(args.load)
         elif file_type == "json":
-            machine, temp_objects,temp_actions,temp_default = load_json(args.load)
-            if(temp_objects is not None or temp_actions is not None or temp_default is not None):
+            machine, temp_objects, temp_events, temp_default = load_json(args.load)
+            if (
+                temp_objects is not None
+                or temp_events is not None
+                or temp_default is not None
+            ):
                 objects = temp_objects
-                actions = temp_actions
+                events = temp_events
                 default = temp_default
         else:
             print("error: file type not supported")
@@ -244,26 +250,27 @@ def main():
         if args.print == "objects":
             for key, value in objects.items():
                 print(f"{key}: {value.to_dict()}")
-        elif args.print == "actions":
-            for index, value in enumerate(actions, start=1):
+        elif args.print == "events":
+            for index, value in enumerate(events, start=1):
                 print(index, value)
         elif args.print == "default":
             units = ["mW", "dBi", "Ohm"]  # for display purpose
             for i, (key, value) in enumerate(default.items()):
                 print(f"{key}: {value} {units[i]}")
 
-    if args.action:  # adds an action
-        action = args.action
-        if action[1] not in objects:
-            print("error, unkown tag:", action[1])
-        elif action[2] not in objects:
-            print("error, unkown tag:", action[2])
+    # Events will be reconfigure lateer to work along side events.json
+    if args.event:  # adds an event
+        event = args.event
+        if event[1] not in objects:
+            print("error, unkown tag:", event[1])
+        elif event[2] not in objects:
+            print("error, unkown tag:", event[2])
         else:
-            times = [a[0] for a in actions]
-            position = bisect.bisect_left(times, action[0])
-            actions.insert(position, action)
-            actions.append(args.action)
-    save_config(objects, actions, default)
+            times = [a[0] for a in events]
+            position = bisect.bisect_left(times, event[0])
+            events.insert(position, event)
+            events.append(args.event)
+    save_config(objects, events, default)
 
 
 if __name__ == "__main__":
